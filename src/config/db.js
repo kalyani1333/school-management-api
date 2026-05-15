a8734521e2dd3c6518a28dd6a4374bdf54a82ef6
MKALYANI33 <230101120003@centurionuniv.edu.in>
2026-05-15 09:03:51 +0530

    update db config

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,12 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectTimeout: 60000,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 connection.connect((err) => {
